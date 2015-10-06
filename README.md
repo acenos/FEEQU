c92f0f808d2c0aeedc723b658e9761ccbf7562aa
acenos <gabrielfezd@gmail.com>
2015-10-06 16:23:45 +0200

    Read map from map.txt

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 * Right now the program expects to find the sprites in the folder img. I am 
 not adding the images to the repository because it is a bad practice.
 Instead I have added a file with the urls of the images that the program needs
+
+## Map file
+* The map will be read from the file map.txt
+* This file will be composed of integers and commas
+* The first two integers will be the size of the map [W,H]
+* The following two integers the initial position of the hero [X,Y]
+* The rest of the integers will be the tiles of the map. 
+If there are less tiles that there should be, ther rest will be Water
+
--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 #include "game.hpp"
 #include <stdio.h>
 #include <cassert>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 #define IMG_TERRAIN "img/maptiles30part1va1nl5.png" 
 #define IMG_SPRITES "img/47384.png" 
+#define MAP_FILE "map.txt"
 #define N_SPRITES 5
 #define RED 0
 #define BLUE 1
 #define GREEN 2
 #define YELLOW 3
-#define WATER 0
-#define GRASS 1
-#define MONTAIN 2
-#define FOREST 3
-#define HERO 4
 #define TILE_W 16 
 #define TILE_H 16
 #define MAP_W 15
@@ -26,7 +26,41 @@ sf::Texture* textures;
 
 void init_data()
 {
-	game.new_map(MAP_W,MAP_H);
+    string line, full_text;
+    ifstream map_file(MAP_FILE);
+    if(map_file.is_open())
+    {   
+        while(getline(map_file,line))full_text.append(line);
+        int i,j;
+        int w,h;
+        i = full_text.find_first_of(",");
+        w = atoi(full_text.substr(0,i).c_str());
+        j=i+1;
+        i = full_text.find_first_of(",",j);
+        h = atoi(full_text.substr(j,i).c_str());
+        game.new_map(w,h);
+        j=i+1;
+        i = full_text.find_first_of(",",j);
+        game.hero_x = atoi(full_text.substr(j,i).c_str());
+        j=i+1;
+        i = full_text.find_first_of(",",j);
+        game.hero_y = atoi(full_text.substr(j,i).c_str());
+        j=i+1;
+        i = full_text.find_first_of(",",j);
+        for(int k = 0; k < w*h && i < full_text.npos; k++)
+        {
+            game.map[k]=atoi(full_text.substr(j,i).c_str());
+            j=i+1;
+            i = full_text.find_first_of(",",j);
+        }
+        printf("%i,%i\n",game.hero_x,game.hero_y);
+    }
+    else
+    {
+        printf("Error opening map file/nUsing default map");
+	    game.new_map(MAP_W,MAP_H);
+	    for(int i = 0; i < game.map_h*game.map_w; i++) game.map[i] = i % 4;
+    }
     sf::Color sprite_mask = sf::Color(128,160,128);
     sf::Vector2f sprite_coord[] = {
                                 sf::Vector2f(195,181), 
@@ -40,20 +74,20 @@ void init_data()
                                 sf::Vector2f(1,103)
                               };
     unsigned int idle_cycle[] = {0,1,2};
-	for(int i = 0; i < game.map_h*game.map_w; i++) game.map[i] = i % 4;
-    game.tilemap.load(IMG_TERRAIN, tex_coord, 4, sf::Vector2u(TILE_W,TILE_H), game.map, MAP_W, MAP_H);
+    game.tilemap.load(IMG_TERRAIN, tex_coord, 4, sf::Vector2u(TILE_W,TILE_H), game.map, game.map_w, game.map_h);
     game.hero_sprite.load(IMG_SPRITES,sprite_coord, 3, sf::Vector2u(TILE_W,TILE_H), sprite_mask);
+    game.hero_sprite.move(game.hero_x,game.hero_y);
     game.hero_sprite.set_idle(idle_cycle, 3);
 	
 }
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(MAP_W * TILE_W, MAP_H * TILE_H), "FEEQU");
+	init_data();
+	sf::RenderWindow window(sf::VideoMode(game.map_w * TILE_W, game.map_h * TILE_H), "FEEQU");
 	window.setFramerateLimit(60); 
 	sf::Sprite sprite_canvas;
 	sf::Clock clock;
-	init_data();
 	bool force_refresh=true;
 	while (window.isOpen())
 	{
